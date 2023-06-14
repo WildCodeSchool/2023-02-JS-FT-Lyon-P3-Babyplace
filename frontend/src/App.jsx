05e32dd2fdc3312e1940e17ff514cfc16296d144
Francois Chavallard <francois.chavallard@gmail.com>
2023-06-14 11:39:25 +0200

    mise en place des routes Home Particuliers et Pro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Particuliers from "./pages/Particuliers";
+import Pro from "./pages/Pro";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>C'est parti !</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pro" element={<Pro />} />
+        <Route path="/particuliers" element={<Particuliers />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import styles from "./Home.module.css";
+import baby from "../assets/images/baby-6087761_1280.jpg";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.design}>
+        <h1>Babyplace</h1>
+        <h2>Faites garder vos enfants par des professionnels.</h2>
+        <img src={baby} alt="baby playing" />
+      </div>
+      <div className={styles.redirectWindow}>
+        <div className={styles.pro}>
+          <p>Je suis un professionnel : </p>
+          <Link to="/pro">
+            <Button variant="contained">Accéder au site pro</Button>
+          </Link>
+        </div>
+        <div className={styles.particuliers}>
+          <p>Je suis un particulier : </p>
+          <Link to="/particuliers">
+            <Button variant="contained">Accéder au site particuliers</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
