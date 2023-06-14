0d2a1e7c934637612e11c8f41f39f7d7a6227dc0
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-06-14 09:57:29 +0200

    routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Particulier from "./pages/Particulier";
 import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>C'est parti !</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/particulier" element={<Particulier />} />
+        <Route path="/pro-register" element={<ProRegister />} />
+        <Route path="/pro-dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
