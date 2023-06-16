5e5502c10ca9d4f301e364f0cb37a99ff6fc2b2b
HazeFury <mab.dev69@gmail.com>
2023-06-16 16:19:05 +0200

    finished the dashboard routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import Particulier from "./pages/Particulier";
 import ProRegister from "./pages/ProRegister";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
+import Calendar from "./components/ProDashboard/Calendar/Calendar";
+import Orders from "./components/ProDashboard/Orders/Orders";
+import UserAuth from "./components/ProDashboard/UserAuth/UserAuth";
+import DashboardHome from "./components/ProDashboard/DashboardHome/DashboardHome";
+import ModifyData from "./components/ProDashboard/ModifyData/ModifyData";
+
 import "./App.css";
 
 function App() {
@@ -12,7 +18,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/particulier" element={<Particulier />} />
         <Route path="/pro-register" element={<ProRegister />} />
-        <Route path="/pro-dashboard" element={<Dashboard />} />
+        <Route path="/pro-dashboard/" element={<Dashboard />}>
+          <Route index element={<DashboardHome />} />
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="authentification" element={<UserAuth />} />
+          <Route path="modify" element={<ModifyData />} />
+        </Route>
       </Routes>
     </div>
   );
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "../components/ProDashboard/NavBar/NavBar";
 import Header from "../components/ProDashboard/Header/Header";
-import DashboardHome from "../components/ProDashboard/DashboardHome/DashboardHome";
-import Calendar from "../components/ProDashboard/Calendar/Calendar";
-import Orders from "../components/ProDashboard/Orders/Orders";
-import UserAuth from "../components/ProDashboard/UserAuth/UserAuth";
-import ModifyData from "../components/ProDashboard/ModifyData/ModifyData";
 
 import "../App.css";
 
@@ -16,13 +11,7 @@ export default function Dashboard() {
       <NavBar />
       <Header />
       <div className="dashboard_box">
-        <Routes>
-          <Route path="/" element={<DashboardHome />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/authentification" element={<UserAuth />} />
-          <Route path="/modify" element={<ModifyData />} />
-        </Routes>
+        <Outlet />
       </div>
     </div>
   );
