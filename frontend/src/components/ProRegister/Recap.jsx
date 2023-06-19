e0b60ce6884ca17f3e5514d16ed44c96bdec3faf
Francois Chavallard <francois.chavallard@gmail.com>
2023-06-19 14:17:42 +0200

    responsive de la page register

--- frontend/src/components/ProRegister/Recap.jsx
+++ frontend/src/components/ProRegister/Recap.jsx
@@ -1,37 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Container } from "@mui/material";
 import styles from "./Recap.module.css";
 
 function Recap({ registerInfo, fieldsToComplete, setActiveField }) {
   return (
     <div className={styles.recapPage}>
-      <p>Voici le résumé de vos informations</p>
-      <div className={styles.recapBlock}>
-        {fieldsToComplete.map((field) => {
-          return (
-            <TextField
-              key={field.field}
-              label={field.field}
-              disabled
-              content={field.field}
-              onClick={setActiveField(field.field)}
-            />
-          );
-        })}
-      </div>
-      <div className={styles.recapFooter}>
-        <Button
-          variant="contained"
-          disabled={
-            Object.values(registerInfo).includes(null) ||
-            Object.values(registerInfo).includes([]) ||
-            Object.values(registerInfo).includes("")
-          }
-        >
-          Terminer
-        </Button>
-      </div>
+      <Container>
+        <div className={styles.text}>
+          <p>Voici le résumé de vos informations</p>
+        </div>
+
+        <div className={styles.recapBlock}>
+          {fieldsToComplete.map((field) => {
+            return (
+              <TextField
+                key={field.field}
+                label={field.field}
+                disabled
+                content={field.field}
+                onClick={setActiveField(field.field)}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  mt: 1,
+                }}
+              />
+            );
+          })}
+        </div>
+        <div className={styles.recapFooter}>
+          <Button
+            variant="contained"
+            disabled={
+              Object.values(registerInfo).includes(null) ||
+              Object.values(registerInfo).includes([]) ||
+              Object.values(registerInfo).includes("")
+            }
+          >
+            Terminer
+          </Button>
+        </div>
+      </Container>
     </div>
   );
 }
