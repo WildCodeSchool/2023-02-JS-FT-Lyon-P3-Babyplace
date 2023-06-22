606cbb33c2ed35c7e71a74be4570ad7304566473
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-06-22 22:44:36 +0200

    form css

--- frontend/src/components/Particuliers/Form/FormParent.jsx
+++ frontend/src/components/Particuliers/Form/FormParent.jsx
@@ -41,109 +41,88 @@ export default function FormParent() {
   };
   return (
     <div className={style.card}>
-      <div>
-        <div className={style.header_card}>
-          <button
-            type="button"
-            className={style.button_back}
-            onClick={() => navigate(-1)}
-          >
-            <ArrowBackIosNewIcon />
-          </button>
-          <div className={style.name_type}>
-            <h2>Création de compte</h2>
-          </div>
+      <div className={style.header_card}>
+        <button
+          type="button"
+          className={style.button_back}
+          onClick={() => navigate(-1)}
+        >
+          <ArrowBackIosNewIcon />
+        </button>
+        <div className={style.name_type}>
+          <h2>Création de compte</h2>
         </div>
-        <form className={style.form} onSubmit={handleSubmit}>
-          <div>
-            <input
-              type="text"
-              name="lastname"
-              placeholder="Nom"
-              value={lastname}
-              onChange={(e) => setLastname(e.target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="firstname"
-              placeholder="Prénom"
-              value={firstname}
-              onChange={(e) => setFirstname(e.target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="date"
-              name="birthdate"
-              placeholder="Date de naissance"
-              value={birthdate}
-              onChange={(e) => setBirthdate(e.target.value)}
-            />
-          </div>
-
-          <div>
-            <input
-              type="email"
-              name="mail_address"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="password"
-              placeholder="Mot de passe"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="address"
-              placeholder="Numéro et nom de voie"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="number"
-              name="postcode"
-              placeholder="Code postal"
-              value={postCode}
-              onChange={(e) => setPostCode(e.target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="city"
-              placeholder="Ville"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="number"
-              name="phone_number"
-              placeholder="Téléphone mobile"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-          </div>
-          <div>
-            <button type="submit" className={style.button}>
-              Créer un compte
-            </button>
-          </div>
-        </form>
       </div>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="lastname"
+          placeholder="Nom"
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+        />
+        <input
+          type="text"
+          name="firstname"
+          placeholder="Prénom"
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <input
+          type="text"
+          name="birthdate"
+          placeholder="Date de naissance"
+          value={birthdate}
+          pattern="\d{2}/\d{2}/\d{4}"
+          onChange={(e) => setBirthdate(e.target.value)}
+        />
+
+        <input
+          type="email"
+          name="mail_address"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="Mot de passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="text"
+          name="address"
+          placeholder="Numéro et nom de voie"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <input
+          type="number"
+          name="postcode"
+          placeholder="Code postal"
+          value={postCode}
+          onChange={(e) => setPostCode(e.target.value)}
+        />
+        <input
+          type="text"
+          name="city"
+          placeholder="Ville"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <input
+          type="number"
+          name="phone_number"
+          placeholder="Téléphone mobile"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <button type="submit" className={style.button}>
+          Créer un compte
+        </button>
+      </form>
     </div>
   );
 }
