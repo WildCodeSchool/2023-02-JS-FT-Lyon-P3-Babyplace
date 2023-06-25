cf70945ac89fe946fcf7c6fce215a3ea41a07b1f
Francois Chavallard <francois.chavallard@gmail.com>
2023-06-25 11:54:45 +0200

    ajout d'un bouton back dans la partie register + prop home pour ne pas afficher le bouton back dans la page d'accueil

--- frontend/src/components/DesignWelcome.jsx
+++ frontend/src/components/DesignWelcome.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { useNavigate } from "react-router-dom";
 import styles from "./DesignWelcome.module.css";
 import baby from "../assets/images/baby-6087761_1280.jpg";
 
-function DesignWelcome() {
+function DesignWelcome({ home }) {
   const navigate = useNavigate();
 
   return (
     <div className={styles.design}>
-      <button
-        type="button"
-        className={styles.button_back}
-        onClick={() => navigate(-1)}
-      >
-        <ArrowBackIosNewIcon />
-      </button>
+      {home ? null : (
+        <button
+          type="button"
+          className={styles.button_back}
+          onClick={() => navigate(-1)}
+        >
+          <ArrowBackIosNewIcon />
+        </button>
+      )}
 
       <h1>Babyplace</h1>
       <h2>Faites garder vos enfants par des professionnels.</h2>
@@ -25,3 +28,7 @@ function DesignWelcome() {
 }
 
 export default DesignWelcome;
+
+DesignWelcome.propTypes = {
+  home: PropTypes.bool.isRequired,
+};
