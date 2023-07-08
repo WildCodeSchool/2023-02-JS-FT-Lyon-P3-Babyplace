0d48f21a4360688c104fbbdea08fafe436c9a5c7
Francois Chavallard <francois.chavallard@gmail.com>
2023-07-08 15:53:08 +0200

    révision des routes et conditions d'affichage

--- frontend/src/pages/Particulier.jsx
+++ frontend/src/pages/Particulier.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { useUserContext } from "../contexts/UserContext";
-import Login from "../components/Login";
 import NavBarParent from "../components/Particuliers/Navbar/NavBarParent";
 
 export default function Particulier() {
-  const { user } = useUserContext();
-
   return (
     <div>
-      {user?.role === "parent" ? (
-        <>
-          <div>
-            <Outlet />
-          </div>
-          <NavBarParent />
-        </>
-      ) : (
-        <Login userType="parent" />
-      )}
+      <div>
+        <Outlet />
+      </div>
+      <NavBarParent />
     </div>
   );
 }
